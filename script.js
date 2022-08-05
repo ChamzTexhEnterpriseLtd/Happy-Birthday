fe0382ef0b098c0161d94a22946eb21dd47893c4
Chamanga David <66671916+ChamzTexhEnterpriseLtd@users.noreply.github.com>
2022-08-06 02:39:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter Fullname");
+ var b=prompt("Enter Date of birth");
+  	if(a== "Rosaria Mwila" && b=="10-08-1999")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
